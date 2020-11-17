447e3583dae7ff469ad2e52a0447b290d0682d8e
Christy Patrick <christy06p@msn.com>
2020-11-16 22:10:17 -0600

    starter code for the bar plot

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,13 @@
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
 d3.json("data/samples.json").then((importedData) => {
-    console.log(importedData);
+    //console.log(importedData);
     var data = importedData;
 
     // Sort the data array using the sample_values value
   // data.sort(function(a, b) {
   //   return parseFloat(b.sample_values) - parseFloat(a.sample_values);
   });
-
-// function updatePage(){
-//   
-
-//   var dropdownID = dropdown.property("id");
-//   var dropdownValue = dropdown.property("value")
-//   console.log(dropdownValue);
-// }
 
 function subjectID(){
 
@@ -34,44 +26,60 @@ function subjectID(){
   });
 };
 
-//
-subjectID();
-
+//filter page by the dropdown?
 
 //sample_values as bar chart values
 //out_ids as labels for the bar chart
 //otu_labels as the hovertext for the chart
-  // values in dropdown = names
 
-  // // Slice the first 10 objects for plotting
-  // data = data.slice(0, 10);
+//Samples: Array
+//ID (like 940)
+//otu_ids
+//sample_values for each otu_id
 
-  // // Reverse the array due to Plotly's defaults
-  // data = data.reverse();
+var ids = importedData.samples.id
+var otu_ids = importedData.samples.otu_ids;
+var sample_values = importedData.samples.sample_values;
 
-  // // Trace1 for the Sample Data
-  // var trace1 = {
-  //   x: data.map(row => row.sample_values),
-  //   y: data.map(row => row.otu_ids),
-  //   text: data.map(row => row.otu_ids),
-  //   name: "Greek",
-  //   type: "bar",
-  //   orientation: "h"
-  // };
 
-  // // data
-  // var chartData = [trace1];
+// Slice the first 10 objects for plotting
+otu_ids = otu_ids.slice(0, 10);
 
-  // // Apply the group bar mode to the layout
-  // var layout = {
-  //   title: "Greek gods search results",
-  //   margin: {
-  //     l: 100,
-  //     r: 100,
-  //     t: 100,
-  //     b: 100
-  //   }
-  // };
+  // Trace1 for the Sample Data
+  var trace1 = {
+    x: data.map(row => row.sample_values),
+    y: data.map(row => row.otu_ids),
+    text: data.map(row => row.otu_ids),
+    name: "Samples",
+    type: "bar",
+    orientation: "h"
+  };
 
-  // // Render the plot to the div tag with id "plot"
-  // Plotly.newPlot("bar", chartData, layout);
+  // data
+  var chartData = [trace1];
+
+  // Apply the group bar mode to the layout
+  var layout = {
+    title: "Placeholder title",
+    xaxis: { title: "Placeholder X" },
+    yaxis: { title: "Placeholder Y"}
+    margin: {
+      l: 100,
+      r: 100,
+      t: 100,
+      b: 100
+    }
+  };
+
+  // Render the plot to the div tag with id "plot"
+  Plotly.newPlot("bar", chartData, layout);
+
+  // function updatePage(){
+//   
+
+//   var dropdownID = dropdown.property("id");
+//   var dropdownValue = dropdown.property("value")
+//   console.log(dropdownValue);
+// }
+//
+subjectID();
