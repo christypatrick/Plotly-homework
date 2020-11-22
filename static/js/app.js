d074068c1adfd69710b758173fb930d7c8c770d1
Christy Patrick <christy06p@msn.com>
2020-11-21 23:15:12 -0600

    updated bar chart code

--- static/js/app.js
+++ static/js/app.js
@@ -1,61 +1,50 @@
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
 d3.json("data/samples.json").then((importedData) => {
-    // console.log(importedData);
-    var data = importedData;
+  // console.log(importedData);
+  var data = importedData;
+});
 
-    // Sort the data array using the sample_values value
-  // data.sort(function(a, b) {
-  //   return parseFloat(b.sample_values) - parseFloat(a.sample_values);
-  });
-
-function subjectID(){
+function subjectID() {
 
   // Get a reference to the subject ID dropdown
   var dropdown = d3.select("#selDataset");
 
   // Populate the Subject ID dropdown
   d3.json("data/samples.json").then((importedData) => {
+
     var names = importedData.names;
+
     names.forEach((name) => {
       dropdown
       .append("option")
       .text(name)
       .property("value", name);
     });
-  var initial = names[0];
-  console.log(initial);
-  
 
+  var initial = names[0];
+  charts(initial);
   });
 };
 
-subjectID();
 
-//filter page by the dropdown?
 
-//sample_values as bar chart values
-//out_ids as labels for the bar chart
-//otu_labels as the hovertext for the chart
-
-//Samples: Array
-//ID (like 940)
-//otu_ids
-//sample_values for each otu_id
-
-// var ids = importedData.samples.id
+function optionChanged(changeSample) {
+  charts(changeSample);
+}
 
 
 function charts(sample){
   d3.json("data/samples.json").then((importedData) => {
-    
+
     var samples = importedData.samples;
     var filteredSamples = samples.filter(subject => subject.id == sample);
-    var filtered = filteredSamples[0]
+    var filtered = filteredSamples[0];
     // console.log(filteredSamples);
+
     var otu_ids = filtered.otu_ids;
     var otu_labels = filtered.otu_labels;
-    var sample_values = filtered.sample_values;
+    var sample_values = filtered.sample_values.slice(0,10);
 
   //Trace1 for the Sample Data
     var trace1 = {
@@ -64,7 +53,12 @@ function charts(sample){
       text: otu_labels,
       name: "Samples",
       type: "bar",
-      orientation: "h"
+      orientation: "h",
+      transforms:[{
+        type: "sort",
+        target: "x",
+        order: "descending"
+      }]
     };
 
     // data
@@ -72,27 +66,25 @@ function charts(sample){
 
     // Apply the group bar mode to the layout
     var layout = {
-      title: "Placeholder title",
-      xaxis: { title: "Placeholder X" },
-      yaxis: { title: "Placeholder Y"}
-
+      title: "Top 10 Samples",
+      xaxis: { title: "Sample Values" },
+      yaxis: { title: "OTU IDs"}
   };
 
-  // Render the plot to the div tag with id "plot"
-  Plotly.newPlot("bar", chartData, layout);
+   // Render the plot to the div tag with id "plot"
+   Plotly.newPlot("bar", chartData, layout);
 
 });
 };
 
-barChart();
+subjectID();
+//charts();
 
 
 // // Slice the first 10 objects for plotting
 // otu_ids = otu_ids.slice(0, 10);
 
 //   
-
-
 
   // function updatePage(){
 //   
